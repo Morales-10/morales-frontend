3354e37ae61a05cc629abf7579a0106b8980d30e
Morales-10 <anna450s@easv365.dk>
2021-12-01 10:07:28 +0100

    startpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'startpage', loadChildren: () =>
+    import('./starpage/starpage.module')
+      .then(m => m.StarpageModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/starpage/starpage.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { StarpageRoutingModule } from './starpage-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    StarpageRoutingModule
+  ]
+})
+export class StarpageModule { }
