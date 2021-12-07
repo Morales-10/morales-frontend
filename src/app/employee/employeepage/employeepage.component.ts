3edfe3f933408b8e7b8660a6b74c0dfd8b900d9d
Morales-10 <anna450s@easv365.dk>
2021-12-07 14:37:41 +0100

    employee

--- src/app/employee/employeepage/employeepage.component.ts
+++ src/app/employee/employeepage/employeepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-Morales-BookingSystem-employeepage',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit(): void {
+  }
+
+  bookAppointment():void{
+    this.router.navigateByUrl('')
+  }
+  createNewAccount(): void {
+    this.router.navigateByUrl('auth/create')
+
   }
 
 }
