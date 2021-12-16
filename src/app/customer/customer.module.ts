1c7d8a55db44422cbbcacf8bb0334ee4bfeb8b68
pete9991 <peter.lydeking@gmail.com>
2021-12-16 13:36:03 +0100

    refactoring

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,14 +4,12 @@ import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomersComponent } from './customers/customers.component';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { CustomerBookingComponent } from './customer-booking/customer-booking.component';
 import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     CustomersComponent,
-    CustomerBookingComponent
   ],
   imports: [
     CommonModule,
