d56eb3c40e4f2f5781807ef5baf1b6e86309c3e0
pete9991 <peter.lydeking@gmail.com>
2021-12-19 12:51:18 +0100

    finalizing

--- src/app/customer/customer-routing.module.ts
+++ src/app/customer/customer-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomersComponent} from "./customers/customers.component";
-import {CustomerBookingComponent} from "./customer-booking/customer-booking.component";
 
 const routes: Routes = [
-  {path:'',component:CustomersComponent},
-  {path:'', component:CustomerBookingComponent}
+  {path:'',component:CustomersComponent}
 ];
 
 @NgModule({
